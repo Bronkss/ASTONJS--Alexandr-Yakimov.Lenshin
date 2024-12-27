243280edc326a4e4677161e729c47f4e854187b1
Alex <alexandr.yakimov.heaven@gmail.com>
2024-12-27 13:13:15 +0300

    ASTONJS-3 [feat]: finalized advanced level

--- lesson3-object/advanced-level.js
+++ lesson3-object/advanced-level.js
@@ -4,26 +4,48 @@
 // return …
 // }
 
-function reverseStr(str) {
-  return str.split("").reverse().join("");
-}
+// function reverseStr(str) {
+//   return str.split("").reverse().join("");
+// }
 
-console.log(reverseStr("Hello world"));
+// console.log(reverseStr("Hello world"));
 
-const reverseStrShort = (str) => str.split("").reverse().join("");
+// const reverseStrShort = (str) => str.split("").reverse().join("");
 
-console.log(reverseStrShort("Hello world"));
+// console.log(reverseStrShort("Hello world"));
 
 // Задание 2– Написать функцию глубокого сравнения двух объектов:
 // const obj1 = { here: { is: "on", other: "3" }, object: Z };
 // const obj2 = { here: { is: "on", other: "2" }, object: Z };
 // const deepEqual = (obj1, obj2) => {};
 
-const obj1 = { here: { is: "on", other: "3" }};
+function deepEqual(obj1, obj2) {
+  if (obj1 === obj2) {
+    return true;
+  }
 
-const obj2 = { here: { is: "on", other: "2" }};
+  if (
+    obj1 == null ||
+    typeof obj1 !== "object" ||
+    obj2 == null ||
+    typeof obj2 !== "object"
+  ) {
+    return false;
+  }
 
-const deepEqual = (obj1, obj2) => JSON.stringify(obj1) === JSON.stringify(obj2);
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
 
-console.log(deepEqual(obj1, obj2))
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
